23370f040d761a136e540feafb6e81c4308528d8
Rahul Biswakarma <rahul.id39@gmail.com>
2022-09-26 03:56:00 +0530

    Added package.json script to automate build and serve process

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,11 @@
 import cors from "cors";
+import path from "path";
 import express from "express";
+import { fileURLToPath } from "url";
 import planetRouter from "./routes/planets/planets.router.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(
@@ -9,6 +14,11 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use("/planets", planetRouter);
+
+app.get("/", (req, res) => {
+  express.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 export default app;
